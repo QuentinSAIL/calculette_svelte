fd0e67d5ae1a75240c9fc902161031c77f3b3911
QuentinSAIL <saillardq@gmail.com>
2023-06-27 10:51:39 +0200

    :lipstick: apple style

--- src/App.svelte
+++ src/App.svelte
@@ -20,33 +20,61 @@
 	}
 </script>
 
-<section>
-	<div>
+<section class="calculator">
+	<div class="display">
 		<input type="text" readonly value={numberInput} />
 	</div>
 
-	<div>
+	<div class="keypad">
+		<Keypad label="C" onClick={() => clear()} btnClass="grey very-long" />
+		<Keypad label="/" onClick={() => addToEquation('/')} btnClass="orange" />
+
 		<Keypad label="7" onClick={() => addToEquation('7')} />
 		<Keypad label="8" onClick={() => addToEquation('8')} />
 		<Keypad label="9" onClick={() => addToEquation('9')} />
-		<Keypad label="+" onClick={() => addToEquation('+')} />
-	</div>
-	<div>
+		<Keypad label="*" onClick={() => addToEquation('*')} btnClass="orange" />
+
 		<Keypad label="4" onClick={() => addToEquation('4')} />
 		<Keypad label="5" onClick={() => addToEquation('5')} />
 		<Keypad label="6" onClick={() => addToEquation('6')} />
-		<Keypad label="-" onClick={() => addToEquation('-')} />
-	</div>
-	<div>
+		<Keypad label="-" onClick={() => addToEquation('-')} btnClass="orange" />
+
+
 		<Keypad label="1" onClick={() => addToEquation('1')} />
 		<Keypad label="2" onClick={() => addToEquation('2')} />
 		<Keypad label="3" onClick={() => addToEquation('3')} />
-		<Keypad label="*" onClick={() => addToEquation('*')} />
-	</div>
-	<div>
-		<Keypad label="C" onClick={() => clear()} />
+		<Keypad label="+" onClick={() => addToEquation('+')} btnClass="orange" />
+
 		<Keypad label="0" onClick={() => addToEquation('0')} />
-		<Keypad label="=" onClick={() => calculate()} />
-		<Keypad label="/" onClick={() => addToEquation('/')} />
+		<Keypad label="," onClick={() => addToEquation('.')} btnClass="long" />
+		<Keypad label="=" onClick={() => calculate()} btnClass="orange" />
 	</div>
 </section>
+
+<style>
+	.calculator {
+		width: 240px;
+		margin: 50px auto;
+		background-color: #333;
+		border-radius: 12px;
+		padding: 20px;
+	}
+
+	input {
+		width: 100%;
+		height: 60px;
+		border: none;
+		background-color: #555;
+		color: white;
+		font-size: 30px;
+		padding: 10px;
+		border-radius: 4px;
+		text-align: right;
+	}
+
+	.keypad {
+		display: grid;
+		grid-template-columns: repeat(4, 1fr);
+		grid-gap: 5px;
+	}
+</style>
--- src/components/Keypad.svelte
+++ src/components/Keypad.svelte
@@ -1,15 +1,44 @@
 <script>
     export let label;
     export let onClick;
+    export let btnClass = "";
 </script>
 
-<button on:click={onClick}>{label}</button>
+<button on:click={onClick} class={btnClass}>
+    {label}
+</button>
 
 <style>
     button {
         width: 50px;
         height: 50px;
         font-size: 20px;
-        margin: 2px;
+        border-radius: 50%;
+        border: none;
+        font-weight: bold;
+        background-color: #555;
+        color: white;
+    }
+
+    .orange {
+        background-color: #ff9501;
+        color: black;
+    }
+
+    .grey {
+        background-color: #808080;
+        color: black;
+    }
+
+    .long {
+        grid-column: span 2;
+        border-radius: 25px;
+        width: auto;
+    }
+
+    .very-long {
+        grid-column: span 3;
+        border-radius: 25px;
+        width: auto;
     }
 </style>
